07fb492205e8c4e1279bff405dd3c3612ca482e3
Egor Galkin <gogex300@gmail.com>
2018-06-27 01:21:13 +0300

    Removed unnecessary import statements.

--- src/main/java/com/galkin/rest/RestApplication.java
+++ src/main/java/com/galkin/rest/RestApplication.java
@@ -2,11 +2,7 @@ package com.galkin.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaAuditing
